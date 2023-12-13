c97cdb1fb94797d85b16298239adac2fefc3f381
jangchangjun <100849835+jangchangjun@users.noreply.github.com>
2023-12-14 01:03:06 +0900

    Add files via upload

--- source/script.js
+++ source/script.js
@@ -35,16 +35,39 @@ nextBtnThird.addEventListener("click", function(event){
   progressText[current - 1].classList.add("active");
   current += 1;
 });
-submitBtn.addEventListener("click", function(){
+let isSubmitInProgress = false; // 추가된 부분
+
+document.addEventListener('DOMContentLoaded', function () {
+  
+  // 기존에 등록된 이벤트 리스너를 모두 제거합니다.
+  submitBtn.removeEventListener("click", submitBtnClickHandler);
+
+  // 새로운 이벤트 리스너를 등록합니다.
+  submitBtn.addEventListener("click", submitBtnClickHandler);
+});
+
+function submitBtnClickHandler() {
+  // 추가된 부분 시작
+  if (isSubmitInProgress) {
+    return;
+  }
+
+  isSubmitInProgress = true;
+  // 추가된 부분 끝
+
   bullet[current - 1].classList.add("active");
   progressCheck[current - 1].classList.add("active");
   progressText[current - 1].classList.add("active");
   current += 1;
-  setTimeout(function(){
+
+  setTimeout(function () {
     alert("플레이리스트를 생성합니다");
-    location.href = "playlist.html"; 
+    location.href = "playlist.html";
   }, 800);
-});
+}
+
+
+
 prevBtnSec.addEventListener("click", function(event){
   event.preventDefault();
   slidePage.style.marginLeft = "0%";
